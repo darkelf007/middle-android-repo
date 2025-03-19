5c4fcc411d320f6a7fa014bdae2188dbcd3d5dc2
Anar <dtalievanar@gmail.com>
2025-03-19 15:08:17 +0300

    Задание к модулю № 1

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
@@ -3,6 +3,8 @@ package com.example.androidpracticumcustomview.ui.theme
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.TranslateAnimation
 import android.widget.FrameLayout
 
 /*
@@ -24,17 +26,53 @@ class CustomContainer @JvmOverloads constructor(
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        // TODO
-        // ...
+        for (i in 0 until childCount) {
+            measureChild(getChildAt(i), widthMeasureSpec, heightMeasureSpec)
+        }
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        // TODO
-        // ...
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            val childWidth = child.measuredWidth
+            val childHeight = child.measuredHeight
+            val childLeft = (width - childWidth) / 2
+            val childTop = if (i == 0) 0 else height - childHeight
+
+            child.layout(
+                childLeft,
+                childTop,
+                childLeft + childWidth,
+                childTop + childHeight
+            )
+        }
     }
 
     override fun addView(child: View) {
-        // TODO
-        // ...
+        if (childCount >= 2) throw IllegalStateException("Cannot add more than two children")
+        child.visibility = View.INVISIBLE
+        super.addView(child)
+        animateChild(child, childCount == 1)
+    }
+
+
+    private fun animateChild(view: View, isSecond: Boolean) {
+        val alphaAnimation = AlphaAnimation(0f, 1f).apply {
+            duration = 2000
+            fillAfter = true
+        }
+
+        val translateAnimation = TranslateAnimation(
+            0f, 0f, height / 2f, if (isSecond) height / 2f else -height / 2.1f
+        ).apply {
+            duration = 5000
+            fillAfter = true
+        }
+
+        view.post {
+            view.visibility = View.VISIBLE
+            view.startAnimation(alphaAnimation)
+            view.startAnimation(translateAnimation)
+        }
     }
 }
--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
@@ -1,8 +1,20 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.IntOffset
 
 
 /*
@@ -18,18 +30,57 @@ fun CustomContainerCompose(
     firstChild: @Composable (() -> Unit)?,
     secondChild: @Composable (() -> Unit)?
 ) {
-    // Блок создания и инициализации переменных
-    // ..
-
-    // Блок активации анимации при первом запуске
-    LaunchedEffect(Unit) {
-        // TODO
-        // ...
+    if (listOfNotNull(firstChild, secondChild).size > 2) {
+        throw IllegalStateException("Cannot have more than two children")
     }
 
-    // Основной контейнер
-    Box() {
-        // TODO
-        // ...
+    val alphaAnimation = remember { Animatable(0f) }
+    val offsetAnimation = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        alphaAnimation.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(2000)
+        )
+        offsetAnimation.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(5000, easing = LinearOutSlowInEasing)
+        )
+    }
+
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        val boxHeight = constraints.maxHeight
+
+        firstChild?.let {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .graphicsLayer(alpha = alphaAnimation.value)
+                    .offset {
+                        IntOffset(
+                            x = 0,
+                            y = ((-offsetAnimation.value * boxHeight) / 2).toInt()
+                        )
+                    }
+            ) {
+                it()
+            }
+        }
+
+        secondChild?.let {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .graphicsLayer(alpha = alphaAnimation.value)
+                    .offset {
+                        IntOffset(
+                            x = 0,
+                            y = ((offsetAnimation.value * boxHeight) / 2).toInt()
+                        )
+                    }
+            ) {
+                it()
+            }
+        }
     }
 }
